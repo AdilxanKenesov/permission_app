1ea5aa1943388165e2f5915b55b2e264bb53b36f
Victus <rustamtursunboyev1715@gmail.com>
2026-07-19 08:55:18 +0500

    esefdr;lbrdb

--- app/src/main/java/uz/gita/mypermissionapp/MainActivity.kt
+++ app/src/main/java/uz/gita/mypermissionapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyPermissionAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello, world!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }

78bfeed3866f9d762e694f9ab123588a97c401bf
adilkhan <kenesovadilxan786@gmail.com>
2026-07-19 09:07:56 +0500

    test1

--- app/src/main/java/uz/gita/mypermissionapp/screen/HomeScreen.kt
+++ app/src/main/java/uz/gita/mypermissionapp/screen/HomeScreen.kt
@@ -1,9 +1,20 @@
 package uz.gita.mypermissionapp.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 class HomeScreen {
     @Composable
-    fun newFun(){}
+    fun NewFun(){
+        Scaffold(modifier = Modifier.fillMaxSize()) {paddingValues ->
+            Text(text = "Hello", modifier = Modifier.padding(paddingValues))
+        }
+    }
 // Rustam
+
+
 }
